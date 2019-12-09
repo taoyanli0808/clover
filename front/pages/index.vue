e6be3df0e77f5b5ba1c65af6ac1f73090b60d75f
李成龙 <lichenglong004@ke.com>
2019-12-09 23:01:17 +0800

    增加一个特别实用的轮播

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,39 +1,15 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        clover
-      </h1>
-      <h2 class="subtitle">
-        vue front for clover platform.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <IndexCarousel />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import IndexCarousel from '~/components/IndexCarousel.vue'
 
 export default {
   components: {
-    Logo
+    IndexCarousel
   }
 }
 </script>
