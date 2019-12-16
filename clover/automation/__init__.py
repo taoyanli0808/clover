1cd384d099e0a334c10a9ad803a0b81ad44060b6
李成龙 <lichenglong@lichenglongdeMacBook-Pro.local>
2019-12-16 17:45:38 +0800

    删除UI自动化部分，第一版不发布。

--- clover/automation/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint
-
-from clover.automation.service import Service
-
-automation = Blueprint('automation', __name__, template_folder='templates')
-
-from clover.automation import views
