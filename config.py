ddfeafadd1d4834b6c7a1028d5a4a4017794a45d
wangqingbin <714227638@qq.com>
2019-12-17 00:34:44 +0800

    resolved conflict

--- config.py
+++ config.py
@@ -17,3 +17,20 @@ DATABASE = {
 
 SQLALCHEMY_DATABASE_URI = "mysql://{user}:{pswd}@{host}:{port}/clover?charset=utf8".format(**MYSQL_CONFIG)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+MYSQL_CONFIG = {
+    'user': 'root',
+    'pswd': 'tyl.0808',
+    'host': '127.0.0.1',
+    'port': 3306,
+}
+
+# MySQL config wqb
+DB_CONFIG = {
+    'HOST': "127.0.0.1",
+    'PORT': 3306,
+    'DBNAME': 'clover',
+    'USERNAME': "root",
+    'PASSWORD': "admin123456",
+    'CHARSET': 'utf8',
+}
