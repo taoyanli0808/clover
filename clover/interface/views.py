d2cb93dc87974a1e262337f0c1b3ebbbc308c038
李成龙 <lichenglong004@ke.com>
2020-01-06 22:03:34 +0800

    抽离出处理器，使代码结构更加合理。

--- clover/interface/views.py
+++ clover/interface/views.py
@@ -4,65 +4,13 @@ from flask import request
 
 from clover.views import CloverView
 from clover.interface.service import Service
+from clover.common.executor import Executor
 
 
 class InterfaceView(CloverView):
 
     def __init__(self):
         super(InterfaceView, self).__init__()
-
-    def update(self):
-        pass
-
-    def debug(self):
-        data = request.get_json()
-
-        name = data.get('name', None)
-        if not name:
-            return jsonify({
-                'status': 400,
-                'message': '接口测试用例需要用例名称！',
-                'data': data,
-            })
-
-        method = data.get('method', None)
-        if not method:
-            return jsonify({
-                'status': 400,
-                'message': '接口测试用例需要正确的请求方法，可选值为[GET|POST|PUT|DELETE]！',
-                'data': data,
-            })
-
-        host = data.get('host', None)
-        if not host:
-            return jsonify({
-                'status': 400,
-                'message': '接口测试用例需要指定域名，例如[https://github.com]！',
-                'data': data,
-            })
-
-        path = data.get('path', None)
-        if not path:
-            return jsonify({
-                'status': 400,
-                'message': '接口测试用例需要指定路径，例如[/taoyanli0808/clover]！',
-                'data': data,
-            })
-
-        try:
-            service = Service()
-            data = service.execute(data)
-            return jsonify({
-                'status': 0,
-                'message': 'ok',
-                'data': data,
-            })
-        except Exception as error:
-            return jsonify({
-                'status': 500,
-                'message': '运行时错误，请联系管理员！',
-                'data': {}
-            })
 
     def create(self):
         data = request.get_json()
@@ -106,36 +54,6 @@ class InterfaceView(CloverView):
                 'data': data
             })
 
-    def trigger(self):
-        # 这里支持GET与POST请求，获取参数方法不同。
-        if request.method == 'GET':
-            data = request.values.to_dict()
-        else:
-            data = request.get_json()
-
-        cases = data.get('cases', None)
-        if not cases:
-            return jsonify({
-                'status': 400,
-                'message': 'invalid parameter [cases]',
-                'data': data,
-            })
-
-        try:
-            service = Service()
-            result = service.trigger(data)
-            return jsonify({
-                'status': 0,
-                'message': 'ok',
-                'data': result,
-            })
-        except Exception as error:
-            return jsonify({
-                'status': 500,
-                'message': str(error),
-                'data': data
-            })
-
     def delete(self):
 
         data = request.get_json()
@@ -161,6 +79,9 @@ class InterfaceView(CloverView):
                 'data': data
             })
 
+    def update(self):
+        pass
+
     def search(self):
 
         if request.method == 'GET':
@@ -176,6 +97,88 @@ class InterfaceView(CloverView):
                 'message': 'ok',
                 'data': result,
                 'total': count,
+            })
+        except Exception as error:
+            return jsonify({
+                'status': 500,
+                'message': str(error),
+                'data': data
+            })
+
+    def debug(self):
+        data = request.get_json()
+
+        name = data.get('name', None)
+        if not name:
+            return jsonify({
+                'status': 400,
+                'message': '接口测试用例需要用例名称！',
+                'data': data,
+            })
+
+        method = data.get('method', None)
+        if not method:
+            return jsonify({
+                'status': 400,
+                'message': '接口测试用例需要正确的请求方法，可选值为[GET|POST|PUT|DELETE]！',
+                'data': data,
+            })
+
+        host = data.get('host', None)
+        if not host:
+            return jsonify({
+                'status': 400,
+                'message': '接口测试用例需要指定域名，例如[https://github.com]！',
+                'data': data,
+            })
+
+        path = data.get('path', None)
+        if not path:
+            return jsonify({
+                'status': 400,
+                'message': '接口测试用例需要指定路径，例如[/taoyanli0808/clover]！',
+                'data': data,
+            })
+
+        try:
+            # service = Service()
+            # data = service.execute(data)
+            executor = Executor()
+            data = executor.execute(data)
+            return jsonify({
+                'status': 0,
+                'message': 'ok',
+                'data': data,
+            })
+        except Exception as error:
+            return jsonify({
+                'status': 500,
+                'message': '运行时错误，请联系管理员！',
+                'data': {}
+            })
+
+    def trigger(self):
+        # 这里支持GET与POST请求，获取参数方法不同。
+        if request.method == 'GET':
+            data = request.values.to_dict()
+        else:
+            data = request.get_json()
+
+        cases = data.get('cases', None)
+        if not cases:
+            return jsonify({
+                'status': 400,
+                'message': 'invalid parameter [cases]',
+                'data': data,
+            })
+
+        try:
+            service = Service()
+            result = service.trigger(data)
+            return jsonify({
+                'status': 0,
+                'message': 'ok',
+                'data': result,
             })
         except Exception as error:
             return jsonify({
