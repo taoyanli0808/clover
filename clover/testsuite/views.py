ad55419bfa3b145eff3fb1131d77b5c5ef84c821
李成龙 <lichenglong@lichenglongdeMacBook-Pro.local>
2020-01-07 11:57:30 +0800

    去掉注视

--- clover/testsuite/views.py
+++ clover/testsuite/views.py
@@ -14,36 +14,20 @@ class TestSuiteView(CloverView):
     def create(self):
         data = request.get_json()
 
-        service = Service()
-        result = service.create(data)
-        if isinstance(result, tuple):
-            code, msg = result
+        try:
+            service = Service()
+            id = service.create(data)
+            return jsonify({
+                'status': 0,
+                'message': '创建测试套件成功，套件ID[{}]'.format(id),
+                'data': id
+            })
+        except Exception as error:
             return jsonify({
                 'status': 500,
-                'message': msg,
-                'data': code
+                'message': '当创建套件时发生异常，请联系管理员！',
+                'data': str(error)
             })
-
-        return jsonify({
-            'status': 0,
-            'message': '创建测试套件成功，套件ID[{}]'.format(id),
-            'data': result
-        })
-
-        # try:
-        #     service = Service()
-        #     id = service.create(data)
-        #     return jsonify({
-        #         'status': 0,
-        #         'message': '创建测试套件成功，套件ID[{}]'.format(id),
-        #         'data': id
-        #     })
-        # except Exception as error:
-        #     return jsonify({
-        #         'status': 500,
-        #         'message': '当创建套件时发生异常，请联系管理员！',
-        #         'data': str(error)
-        #     })
 
     def delete(self):
         data = request.get_json()
