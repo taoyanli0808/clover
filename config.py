9f00900cd9f25b1d8c428b8d4cbc4c49e29a9953
wangqingbin <714227638@qq.com>
2020-01-08 21:45:06 +0800

    fixbug ct2

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:admin123456@127.0.0.1:3306/clover?charset=utf8'
+MYSQL = {
+    'user': 'clover',
+    'pswd': '52.clover',
+    'host': '127.0.0.1',
+    'port': '3306',
+}
+SQLALCHEMY_DATABASE_URI = 'mysql://{user}:{pswd}@{host}:{port}/clover?charset=utf8'.format(**MYSQL)
 SQLALCHEMY_TRACK_MODIFICATIONS=True
