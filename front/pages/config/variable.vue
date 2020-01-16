e805a834a6e4de25b38d58ea860df9df7ab548dc
李成龙 <lichenglong004@ke.com>
2020-01-16 22:03:10 +0800

    空格导致的warning

--- front/pages/config/variable.vue
+++ front/pages/config/variable.vue
@@ -121,7 +121,7 @@
     >
       <el-form ref="form" label-width="80px">
         <el-form-item label="团队与项目">
-         <TeamProjectCascader v-on:selectedTeamProject="selectedTeamProject" />
+          <TeamProjectCascader v-on:selectedTeamProject="selectedTeamProject" />
         </el-form-item>
         <el-form-item label="负责人">
           <el-input v-model="owner" />
