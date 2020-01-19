94131dda2db57146529679b7115783d57041dc10
李成龙 <lichenglong@lichenglongdeMacBook-Pro.local>
2020-01-19 18:22:22 +0800

    修改图标和title

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'clover测试平台' || process.env.npm_package_name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
