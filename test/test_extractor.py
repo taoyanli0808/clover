80c4263c765114271218565333867d0c82193f68
李成龙 <lichenglong004@ke.com>
2020-02-02 12:22:31 +0800

    修复坏掉的测试用例

--- test/test_extractor.py
+++ test/test_extractor.py
@@ -1,27 +1,27 @@
 
 import unittest
 
-from clover.common import Extract
+from clover.common.extractor import Extractor
 
 
 class TestExpect(unittest.TestCase):
 
     def setUp(self):
-        self.extract = Extract()
+        self.extractor = Extractor()
 
     def test_delimiter_bad_expression_type(self):
-        result = self.extract.extract_by_delimiter("", 3, "")
+        result = self.extractor.extract_by_delimiter("", 3, "")
         self.assertIsNone(result)
 
     def test_delimiter_bad_data_string(self):
-        result = self.extract.extract_by_delimiter("123[]", "")
+        result = self.extractor.extract_by_delimiter("123[]", "")
         self.assertIsNone(result)
 
     def test_delimiter_bad_data_string(self):
         data = '{"status": 0, "message": "ok", "data": [{"price": "13"}, {"price": "15"}]}'
-        result = self.extract.extract_by_delimiter(data, "status")
+        result = self.extractor.extract_by_delimiter(data, "status")
         self.assertEqual(0, result)
-        result = self.extract.extract_by_delimiter(data, "data.-1.price")
+        result = self.extractor.extract_by_delimiter(data, "data.-1.price")
         self.assertEqual("15", result)
 
 
