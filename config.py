cd999047239806e8cf363b6ee5e168ce3600861d
李成龙 <lichenglong004@ke.com>
2020-02-23 21:01:19 +0800

    #27 开发创建接口用例插件

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 
+# Clover全局配置
 DEBUG = True
-VERSION = '0.4.1'
+VERSION = '0.5.1'
 
+# MySQL数据库配置
 MYSQL = {
     'user': 'clover',
     'pswd': '52.clover',
@@ -11,9 +13,13 @@ MYSQL = {
 SQLALCHEMY_DATABASE_URI = 'mysql://{user}:{pswd}@{host}:{port}/clover?charset=utf8'.format(**MYSQL)
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
-CELERY_BROKER_URL='redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/1'
+# celery配置
+CELERY_HOST = 'redis://127.0.0.1:6379/{}'
+CELERY_TIMEZONE = 'Asia/Shanghai'
+CELERY_BROKER_URL = CELERY_HOST.format(0)
+CELERY_RESULT_BACKEND = CELERY_HOST.format(1)
 
+# 功能控制，True则生效，False则无效
 MODULE = {
     'join': True,   #展示加入我们
     'task': False,  #开发中的定时任务
