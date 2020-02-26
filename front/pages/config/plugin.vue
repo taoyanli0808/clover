506ca07b7176e361e6a9c049d91f28d93d28672d
李成龙 <lichenglong@lichenglongdeMacBook-Pro.local>
2020-02-26 16:39:53 +0800

    拆分接口导入前端

--- front/pages/config/plugin.vue
+++ front/pages/config/plugin.vue
@@ -1,34 +1,58 @@
 <template>
   <div>
-    <TeamProjectCascader  v-on:selectedTeamProject="selectedTeamProject" />
-    <el-select v-model="plugin" @change="changePlugin" placeholder="请选择插件">
-      <el-option label="postman" value="postman" />
-    </el-select>
-    <el-upload
-      ref="upload"
-      :limit="1"
-      :file-list="fileList"
-      :show-file-list="false"
-      :on-exceed="handleExceed"
-      :http-request="handleUpload"
-      action="null"
-      class="upload-demo"
-      accept=".json"
-      drag
-    >
-      <i class="el-icon-upload" />
-      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div class="el-upload__tip" slot="tip">只能上传postman集合文件</div>
-    </el-upload>
+    <el-row :gutter="20">
+      <el-col span="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>Postman插件</span>
+            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+          </div>
+          <div class="text item">
+            <TeamProjectCascader v-on:selectedTeamProject="selectedTeamProject" />
+            <PostmanPlugin :team="team" :project="project" />
+          </div>
+        </el-card>
+      </el-col>
+      <el-col span="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>Jmeter插件</span>
+            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+          </div>
+          <div class="text item">
+            <TeamProjectCascader v-on:selectedTeamProject="selectedTeamProject" />
+            <JmeterPlugin :team="team" :project="project" />
+          </div>
+        </el-card>
+      </el-col>
+      <el-col span="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>Httprunner插件</span>
+            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+          </div>
+          <div class="text item">
+            <TeamProjectCascader v-on:selectedTeamProject="selectedTeamProject" />
+            <HTTPRunnerPlugin :team="team" :project="project" />
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import TeamProjectCascader from '~/components/TeamProjectCascader.vue'
+import JmeterPlugin from '~/components/plugin/JmeterPlugin.vue'
+import PostmanPlugin from '~/components/plugin/PostmanPlugin.vue'
+import HTTPRunnerPlugin from '~/components/plugin/HTTPRunnerPlugin.vue'
 
 export default {
   components: {
-    TeamProjectCascader
+    TeamProjectCascader,
+    JmeterPlugin,
+    PostmanPlugin,
+    HTTPRunnerPlugin
   },
   data () {
     return {
@@ -43,60 +67,6 @@ export default {
     selectedTeamProject (value) {
       this.team = value.team
       this.project = value.project
-    },
-    changePlugin (value) {
-      this.plugin = value
-    },
-    handleExceed (files, fileList) {
-      this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
-    },
-    handleUpload (params) {
-      console.log(params)
-
-      const formData = new FormData()
-
-      const limit10M = params.file.size / 1024 / 1024 <= 10
-      if (!limit10M) {
-        // 后续这里处理下，超过10M不再继续走上传逻辑。
-        this.$message({
-          type: 'warning',
-          message: '上传的文件大小应该小于10M!',
-          center: true
-        })
-      }
-
-      formData.append('team', this.team)
-      formData.append('project', this.project)
-      formData.append('plugin', this.plugin)
-      formData.append('file', params.file)
-
-      this.$axios.post(
-        '/api/v1/plugin/create',
-        formData,
-        {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        }
-      ).then(function (res) {
-        if (res.data.status === 0) {
-          this.$message({
-            type: 'success',
-            message: res.data.message,
-            center: true
-          })
-        } else {
-          this.$message({
-            type: 'warning',
-            message: res.data.message,
-            center: true
-          })
-        }
-      }).catch(function () {
-        this.$message({
-          type: 'error',
-          message: '服务器偷懒了！',
-          center: true
-        })
-      })
     }
   }
 }
